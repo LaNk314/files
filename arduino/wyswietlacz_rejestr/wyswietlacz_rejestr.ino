0fb1a7832e2a99023bc9b42b1e135a626da09120
LaNk314 <langustek40@gmail.com>
2021-12-15 19:55:15 +0100

    Final version

--- arduino/wyswietlacz_rejestr/wyswietlacz_rejestr.ino
+++ arduino/wyswietlacz_rejestr/wyswietlacz_rejestr.ino
@@ -1,42 +1,53 @@
 int latchPin = 11;
 int clockPin = 9;
 int dataPin = 12;
+int countButton = 3;
 int switchButton = 2;
-int countButton = 4;
 int pressCounter = 0;
 
-byte states[11] = {B01111110, B00010010, B10111100, B10110110, B11010010, B11100110, 
-                      B11101110 ,B00110010 ,B11111110 ,B11110110, B11111111};
+byte states[11] = { B01111110, // 0
+                    B00010010, // 1
+                    B10111100, // 2
+                    B10110110, // 3
+                    B11010010, // 4
+                    B11100110, // 5
+                    B11101110, // 6
+                    B00110010, // 7
+                    B11111110, // 8
+                    B11110110, // 9
+                    B11111111, // 10(full)
+                  };
 
 void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
-  pinMode(2, INPUT);
-  pinMode(4, INPUT);
+  pinMode(switchButton, INPUT_PULLUP);
+  pinMode(countButton, INPUT_PULLUP);
+}
+
+void show(int number){
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, states[number]);  
+  digitalWrite(latchPin, HIGH);
 }
 
 void loop(){
-
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, LSBFIRST, states[pressCounter%10]);
-  digitalWrite(latchPin, HIGH);
-  if(digitalRead(switchButton) == HIGH){                                // oczekiwanie na przejście do stanu liczenia
-    pressCounter = 0;                                       
-    delay(500);
   
+  if(digitalRead(switchButton) == LOW){                                // oczekiwanie na wejście do pętli
+    pressCounter = 0;                                       
+    delay(500);  
     while(1){                                                   
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, LSBFIRST, states[10]);                // zapalone wszystkie znaki informują o uruchomionym zliczaniu
-      digitalWrite(latchPin, HIGH);
-      if(digitalRead(countButton) == HIGH){                             // wciśnięcie countButton inkrementuje wartość licznika
+      show(10);
+      if(digitalRead(countButton) == LOW){                             // wciśnięcie countButton inkrementuje wartość licznika
         delay(500);
         pressCounter ++;
       }
-      if(digitalRead(switchButton) == HIGH){                            //oczekiwanie na przejście spowrotem do stanu wyświetlania
+      if(digitalRead(switchButton) == LOW){                            // oczekiwanie na spełnienie warunku (wciśnięcie switchButton) do wyjścia z pętli
       delay(500);
       break;
       }
     }
+    show(pressCounter%10);
   }  
 }
