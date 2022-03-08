f458dcd97b09904f8e36dee56bc6efd51d8dcd36
LaNk314 <85101748+LaNk314@users.noreply.github.com>
2022-03-08 15:18:21 +0100

    Add files via upload

---
+++ program 2/src/main/java/Hello.java
@@ -0,0 +1,8 @@
+
+
+public class Hello {
+    public static void main(String[] args) {
+        Rectangle figura = new Rectangle(new Point(1, 3), new Point(5,13));
+        System.out.println(figura.area());
+    }
+}
