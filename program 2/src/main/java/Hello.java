969b11581a0c787b4a26dc29a9460e7d42b10c7e
LaNk314 <85101748+LaNk314@users.noreply.github.com>
2022-03-14 22:11:40 +0100

    Delete program 2 directory

--- program 2/src/main/java/Hello.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Hello {
-    public static void main(String[] args) {
-        Rectangle figura = new Rectangle(new Point(1, 3), new Point(5,13));
-        System.out.println(figura.area());
-    }
-}
